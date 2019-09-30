b90c5f07c3e50a1efe79bded6387b2c6773950b6
Leon Kuhne <lxk1170@rit.edu>
2019-09-29 20:47:53 -0400

    created and implemented

--- hw_highlighter.js
+++ hw_highlighter.js
@@ -1,6 +1,6 @@
-let font_size = 10
-let dbname = 'tonydb'
+let font_size = 24
 let DISABLE_PRINT = true
+let DAY_LOOK_AHEAD = 4
 
 function print(text) {
 	if (!DISABLE_PRINT) {
@@ -8,113 +8,51 @@ function print(text) {
 	}
 }
 
-// db handler, retreive as string, handle as object
-function getEntries() {
-	let storage = window.localStorage.getItem(dbname)
-	if (storage == null || storage == "[]") {
-		print('no entries exist')
-		return null
-	} else {
-		try {
-			return JSON.parse(storage)
-		} catch (e) {
-			print('failed to get entries')
-		}
-	}
+function getAssignment(idx) {
+    return $('.d2l-table-cell-last').get(idx)
 }
 
-// get the most frequent entry
-function highest() {
-	let entries = getEntries()
-	let maxCount = 0
-	let highestEntry
-	for (entry in entries) {
-		let count = entries[entry]
-		let keys = entry.split('|')
-		let url = keys[0]
-		let key = keys[1]
-		if (url == window.location.href && count > maxCount) {
-			maxCount = count
-			highestEntry = key
-		}
-	}
-	return highestEntry
-
+// query all assignments on the page
+function getAssignments() {
+    print('getting assignments')
+    
+    let assignments = [] 
+    $('.d2l-table-cell-last').each((index)=>{
+        try { // TODO remove this try catch once verified
+            let str = getAssignment(index).innerText
+            if (!Number.isNaN(Date.parse(str))) {
+                assignments.push(index)
+            }
+        } catch(e) {
+            print('failed to convert date')
+        }
+    })
+    return assignments
 }
 
-let lastPressed = Date.now()
-let DOUBLE_TAP_DELAY = 200 // ms
+function inRange(date, nDays) {
+    let oneDay = 24 * 60 * 60 * 1000;
+    let today = new Date()
+    let dayDiff = (date - today) / oneDay
+    return dayDiff <= nDays && dayDiff >= 0;
+}
+
 function draw() {
-	let key = highest()
+	let assignments = getAssignments()
 
-	if (key) {
-		// insert in comma before spaces
-
-		print('drawing with key: ' + key)
-		$(key).css({
-			"border-color": "green",
-			"border-style": "solid",
-			"border-width": "4px"
-		})
-
-		// TODO add keyboard input listener
-		$("html").keypress((event) => {
-			let now = Date.now()
-			if (event.which == 46 && lastPressed + DOUBLE_TAP_DELAY > now) { // '.' key
-				print('clicking on button: ' + JSON.stringify($(key + ":first")))
-				$(key + ":first").trigger('click') // only take the first element for now TODO
-			}
-			lastPressed = now
-		})
-	}
+    for (idx in assignments) {
+        let assignment = getAssignment(idx)
+        let dueDate = Date.parse(assignment.innerText)
+        if (inRange(dueDate, DAY_LOOK_AHEAD)) {
+	    	$(assignment).parent().css({
+			    "background-color": "#ddeedd",
+	    	})
+        }
+    }
 }
 
-// transaction for incrementing a key value by one
-function bump(pageItem) {
-	let entries = getEntries()
+setTimeout(()=>{
+    draw()
+    print("page loaded")
+}, 100)
 
-	let keys = pageItem.split('|')
-	url = keys[0]
-
-	// only execute on current page
-	if (url == window.location.href) {
-		key = keys[1]
-
-		if (entries) {
-			entries[pageItem] += 1
-			print('incremented entry (' + entries[pageItem] + ')')
-		} else {
-			entries = {}
-			entries[pageItem] = 1
-			print('created entry')
-		}
-
-		// store as string
-		window.localStorage.setItem(dbname, JSON.stringify(entries))
-		print('new count ' + count + 1 + ' for key ' + key)
-	}
-}
-
-
-
-draw()
-
-$('*').on("click", (elem) => {
-	let clicked = $(elem.target)
-
-	let key = "" // <tag> <classes> <id>
-	key += clicked.prop("tagName")
-	key += '.' + clicked.attr('class').replace(/\s/g, '.')
-	let id = clicked.attr('id')
-	key += id ? '#' + id : ''
-
-	if (key) { // store url/key, update value
-		print("button clicked with key: " + key)
-		bump(window.location.href + '|' + key)
-		print(highest())
-		//draw()
-	}
-})
-
-
-print("page loaded")
